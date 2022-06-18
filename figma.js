6931c3f7f38b9361fb94b94792e983a71a8aa540
cyh2020 <50743454+cyh2020@users.noreply.github.com>
2022-06-18 10:50:55 +0800

    Create figma.js

---
+++ figma.js
@@ -0,0 +1,161 @@
+{
+    "children": [
+        {
+            "type": "Group",
+            "x": 0,
+            "y": 0.00002562205926537331,
+            "children": [
+                {
+                    "children": [],
+                    "complex": null,
+                    "h": 128.00012371424893,
+                    "cid": "j9o7zhyf5nqxi4",
+                    "name": "圆",
+                    "type": "Circle",
+                    "w": 127.99999999999955,
+                    "x": 0,
+                    "y": 189.00018267181628,
+                    "constraint": {
+                        "h": 0,
+                        "v": 0
+                    },
+                    "strokeFills": [],
+                    "fills": [
+                        {
+                            "type": 0,
+                            "v": true,
+                            "blend": 1,
+                            "color": [
+                                0.8470588235294118,
+                                0.8470588235294118,
+                                0.8470588235294118,
+                                1
+                            ]
+                        }
+                    ],
+                    "strokeShape": {
+                        "w": 0,
+                        "cap": 2,
+                        "align": 1,
+                        "join": 0,
+                        "dotted": 0,
+                        "interval": 0,
+                        "start": 0,
+                        "end": 0
+                    },
+                    "v": true,
+                    "locked": false,
+                    "exports": [],
+                    "aspectRatio": false,
+                    "blend": 0,
+                    "blur": null,
+                    "mask": null,
+                    "o": 1,
+                    "shadows": [],
+                    "start": 0,
+                    "sweep": 1,
+                    "ratio": 0,
+                    "z": 1
+                },
+                {
+                    "constraint": {
+                        "h": 0,
+                        "v": 0
+                    },
+                    "strokeFills": [
+                        {
+                            "v": true,
+                            "type": 0,
+                            "color": [
+                                0.592156862745098,
+                                0.592156862745098,
+                                0.592156862745098,
+                                1
+                            ],
+                            "blend": 1
+                        }
+                    ],
+                    "fills": [
+                        {
+                            "type": 0,
+                            "v": true,
+                            "blend": 1,
+                            "color": [
+                                0.8470588235294118,
+                                0.8470588235294118,
+                                0.8470588235294118,
+                                1
+                            ]
+                        }
+                    ],
+                    "strokeShape": {
+                        "w": 1,
+                        "cap": 2,
+                        "align": 1,
+                        "join": 0,
+                        "dotted": 0,
+                        "interval": 0,
+                        "start": 0,
+                        "end": 0
+                    },
+                    "v": true,
+                    "locked": false,
+                    "exports": [],
+                    "aspectRatio": false,
+                    "blend": 0,
+                    "blur": null,
+                    "mask": null,
+                    "o": 1,
+                    "shadows": [],
+                    "children": [],
+                    "complex": {
+                        "a": 0.7071064394710835,
+                        "b": -0.7071071229018463,
+                        "c": 0.7071064394710533,
+                        "d": 0.7071071229018765
+                    },
+                    "corner": 0,
+                    "h": 128.00006185713752,
+                    "cid": "j8whmsj5xo9gkm",
+                    "name": "矩形",
+                    "type": "RoundedRect",
+                    "w": 128.00006185713593,
+                    "x": -26.509667991878075,
+                    "y": 64.00006185712297,
+                    "z": 0
+                }
+            ],
+            "complex": null,
+            "cid": "j9ot9pgq61y5qv",
+            "z": 1,
+            "name": "组",
+            "v": true,
+            "locked": false,
+            "exports": [],
+            "aspectRatio": false,
+            "blend": 0,
+            "blur": null,
+            "mask": null,
+            "o": 1,
+            "shadows": [],
+            "fills": [],
+            "strokeFills": [],
+            "strokeShape": {
+                "w": 0,
+                "cap": 2,
+                "align": 1,
+                "join": 0,
+                "dotted": 0,
+                "interval": 0,
+                "start": 0,
+                "end": 0
+            }
+        }
+    ],
+    "cid": "paknsnxr7om0v19c",
+    "name": "页面",
+    "type": "Page",
+    "pageType": 1,
+    "guides": [],
+    "z": 1
+}
